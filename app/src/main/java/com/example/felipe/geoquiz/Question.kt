59cdb28aead13d6966d44d682de37e4fd6a160cf
Felipe de León <f7.deleon@gmail.com>
2018-06-22 15:05:14 -0300

    Added first quiz functionality with the Toast at the top (#3)

---
+++ app/src/main/java/com/example/felipe/geoquiz/Question.kt
@@ -0,0 +1,14 @@
+package com.example.felipe.geoquiz
+
+private val textResId:Int
+private val answerTrue:Boolean
+
+constructor( textResId:Int,  answerTrue:Boolean)
+
+init {
+    this.textResId = textResId
+    this.answerTrue = answerTrue
+}
+
+public textResId:Int
+}
