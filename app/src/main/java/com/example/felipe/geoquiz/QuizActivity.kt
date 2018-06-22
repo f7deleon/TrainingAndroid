8b0f1659ace878eb40267eff98bb050baf475143
f7deleon <felipe@xmartlabs.com>
2018-06-22 14:59:35 -0300

    Fixed style issues

--- app/src/main/java/com/example/felipe/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/example/felipe/geoquiz/QuizActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.view.Gravity
 import android.widget.Button
 import android.widget.Toast
-import java.io.BufferedReader
+
 
 class QuizActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
@@ -15,17 +15,15 @@ class QuizActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
-        trueButton =  findViewById(R.id.btnTrue)
-        trueButton.setOnClickListener { printResult(R.string.correct)  }
+        trueButton = findViewById(R.id.btnTrue)
+        trueButton.setOnClickListener { printResult(R.string.correct) }
 
         falseButton = findViewById(R.id.btnFalse)
-        falseButton.setOnClickListener {printResult(R.string.incorrect) }
-
+        falseButton.setOnClickListener { printResult(R.string.incorrect) }
     }
 
-    fun printResult(message: Int) = with (Toast.makeText(this ,message, Toast.LENGTH_SHORT)) {
-        setGravity(Gravity.TOP,0,200)
+    fun printResult(message: Int) = with(Toast.makeText(this, message, Toast.LENGTH_SHORT)) {
+        setGravity(Gravity.TOP, 0, 200)
         show()
     }
-
 }
