0e46ceaffb2b9e641b0d050a41c2fe88f9e2e210
f7deleon <felipe@xmartlabs.com>
2018-06-25 17:42:08 -0300

    Added cheat activity

---
+++ app/src/main/java/com/example/felipe/geoquiz/CheatActivity.kt
@@ -0,0 +1,69 @@
+package com.example.felipe.geoquiz
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_cheat.*
+
+class CheatActivity : AppCompatActivity() {
+    private var answerIsTrue = false
+    private var answerWasShown = false
+
+    companion object {
+        const val EXTRA_ANSWER_IS_TRUE = "com.example.felipe.geoquiz.answer_is_true"
+        const val EXTRA_ANSWER_SHOWN = "com.example.felipe.geoquiz.answer_shown"
+        const val ANS_INDEX = "ans_index"
+        const val SHW_INDEX = "shw_index"
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+            var intent = Intent(packageContext, CheatActivity::class.java)
+            intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+            return intent
+        }
+
+        fun wasAnswerShown(result: Intent) = result.getBooleanArrayExtra(EXTRA_ANSWER_SHOWN)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cheat)
+
+        if (savedInstanceState != null) {
+            answerIsTrue = savedInstanceState.getBoolean(ANS_INDEX)
+            answerWasShown = savedInstanceState.getBoolean(SHW_INDEX)
+            if (answerWasShown){
+                showAnswer()
+                setAnswerShowResult(true)
+            }
+        }else {
+            answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+        }
+
+        btnShowAnswer.setOnClickListener {
+                showAnswer()
+                setAnswerShowResult(true)
+        }
+    }
+
+    fun showAnswer(){
+        if (answerIsTrue) {
+            txtAnswer.setText(R.string._true)
+        } else {
+            txtAnswer.setText(R.string._false)
+        }
+    }
+
+    fun setAnswerShowResult(isAnswerShown: Boolean) {
+        var data = Intent()
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown)
+        setResult(Activity.RESULT_OK, data)
+        answerWasShown = true
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle?) {
+        super.onSaveInstanceState(savedInstanceState)
+        savedInstanceState?.putBoolean(ANS_INDEX, answerIsTrue)
+        savedInstanceState?.putBoolean(SHW_INDEX, answerWasShown)
+    }
+}
--- app/src/main/java/com/example/felipe/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/example/felipe/geoquiz/QuizActivity.kt
@@ -1,5 +1,7 @@
 package com.example.felipe.geoquiz
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
@@ -13,12 +15,16 @@ class QuizActivity : AppCompatActivity() {
     private val quizController = QuizController()
     private var index = 0
     private lateinit var answered: Array<Boolean>
+    private lateinit var cheated: Array<Boolean>
+
 
     companion object {
         const val KEY_INDEX = "index"
         const val ANS_INDEX = "ans_index"
+        const val CHT_INDEX = "cht_index"
+        const val REQUEST_CODE_CHEAT = 0
     }
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Timber.d("onCreate(Bundle) called")
@@ -31,9 +37,11 @@ class QuizActivity : AppCompatActivity() {
         if (savedInstanceState != null) {
             index = savedInstanceState.getInt(KEY_INDEX)
             answered = savedInstanceState.getBooleanArray(ANS_INDEX).toTypedArray()
+            cheated = savedInstanceState.getBooleanArray(CHT_INDEX).toTypedArray()
             onQuestionAnswered()
         } else {
             answered = Array<Boolean>(quizController.size) { false }
+            cheated = Array<Boolean>(quizController.size) { false }
         }
 
         txtQuestion.setText(quizController.getQuestion(index).textResId)
@@ -62,6 +70,10 @@ class QuizActivity : AppCompatActivity() {
             answered.set(index, true)
             onQuestionAnswered()
         }
+        btnCheat.setOnClickListener {
+            val intent = CheatActivity.newIntent(this@QuizActivity, quizController.getQuestion(index).isTrue)
+            startActivityForResult(intent, REQUEST_CODE_CHEAT)
+        }
     }
 
 
@@ -89,8 +101,10 @@ class QuizActivity : AppCompatActivity() {
         super.onSaveInstanceState(savedInstanceState)
         savedInstanceState?.putInt(KEY_INDEX, index)
         savedInstanceState?.putBooleanArray(ANS_INDEX, answered.toBooleanArray())
+        savedInstanceState?.putBooleanArray(CHT_INDEX, cheated.toBooleanArray())
         Timber.i("onSaveInstanceState")
     }
+
 
     private fun onQuestionAnswered() {
         when (answered[index]) {
@@ -105,11 +119,24 @@ class QuizActivity : AppCompatActivity() {
         }
     }
 
-    private fun showAnswerResult(answer: Boolean) {
-        val answer = when (quizController.checkAnswerResult(answer, index)) {
-            true -> R.string.correct
-            false -> R.string.incorrect
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode != Activity.RESULT_OK) return
+
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            if (data == null) return
+            if(!cheated[index])cheated.set(index, true)
         }
+    }
+
+    private fun showAnswerResult(answer: Boolean) {
+        val answer =
+                if (cheated[index]) R.string.judgment_alert
+                else
+                    when (quizController.checkAnswerResult(answer, index)) {
+                        true -> R.string.correct
+                        false -> R.string.incorrect
+                    }
+
         with(Toast.makeText(this, answer, Toast.LENGTH_SHORT)) {
             setGravity(Gravity.TOP, 0, 200)
             show()
