2e152f48202e4fd3c50dedd5fe1c69ac6cb75d3d
Felipe de León <f7.deleon@gmail.com>
2018-06-25 14:52:17 -0300

    Added Log entries, fixed landscape bug (#5)

--- app/src/main/java/com/example/felipe/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/example/felipe/geoquiz/QuizActivity.kt
@@ -5,30 +5,104 @@ import android.os.Bundle
 import android.view.Gravity
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_quiz.*
+import timber.log.Timber
+import timber.log.Timber.DebugTree
 
 
 class QuizActivity : AppCompatActivity() {
     private val quizController = QuizController()
     private var index = 0
+    private lateinit var answered: Array<Boolean>
+
+    companion object {
+        const val KEY_INDEX = "index"
+        const val ANS_INDEX = "ans_index"
+    }
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Timber.d("onCreate(Bundle) called")
         setContentView(R.layout.activity_quiz)
 
-        txtQuestion.setText(quizController.getQuestion(index))
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
+
+        if (savedInstanceState != null) {
+            index = savedInstanceState.getInt(KEY_INDEX)
+            answered = savedInstanceState.getBooleanArray(ANS_INDEX).toTypedArray()
+            onQuestionAnswered()
+        } else {
+            answered = Array<Boolean>(quizController.size) { false }
+        }
+
+        txtQuestion.setText(quizController.getQuestion(index).textResId)
+
         btnNext.setOnClickListener {
             index = (index + 1) % quizController.size
-            txtQuestion.setText(quizController.getQuestion(index))
+            txtQuestion.setText(quizController.getQuestion(index).textResId)
+            onQuestionAnswered()
         }
         btnPrev.setOnClickListener {
             index = when (index - 1 >= 0) {
                 true -> index - 1
                 false -> quizController.size - 1
             }
-            txtQuestion.setText(quizController.getQuestion(index))
+            txtQuestion.setText(quizController.getQuestion(index).textResId)
+            onQuestionAnswered()
         }
 
-        btnTrue.setOnClickListener { showAnswerResult(true) }
-        btnFalse.setOnClickListener { showAnswerResult(false) }
+        btnTrue.setOnClickListener {
+            showAnswerResult(true)
+            answered.set(index, true)
+            onQuestionAnswered()
+        }
+        btnFalse.setOnClickListener {
+            showAnswerResult(false)
+            answered.set(index, true)
+            onQuestionAnswered()
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        Timber.d("onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.d("onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.d("onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.d("onDestroy() called")
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle?) {
+        super.onSaveInstanceState(savedInstanceState)
+        savedInstanceState?.putInt(KEY_INDEX, index)
+        savedInstanceState?.putBooleanArray(ANS_INDEX, answered.toBooleanArray())
+        Timber.i("onSaveInstanceState")
+    }
+
+    private fun onQuestionAnswered() {
+        when (answered[index]) {
+            true -> {
+                btnTrue.isEnabled = false
+                btnFalse.isEnabled = false
+            }
+            false -> {
+                btnTrue.isEnabled = true
+                btnFalse.isEnabled = true
+            }
+        }
     }
 
     private fun showAnswerResult(answer: Boolean) {
@@ -42,3 +116,4 @@ class QuizActivity : AppCompatActivity() {
         }
     }
 }
+
